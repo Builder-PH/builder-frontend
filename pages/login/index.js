2efed03933e19280b110f80a90526ba3cf1732a5
Nirel Ibarra <62606372+knee-rel@users.noreply.github.com>
2021-10-15 22:03:36 +0800

    initial home and login page

--- pages/login/index.js
+++ pages/login/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
@@ -19,8 +18,7 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 //COMPONENTS
-import Navbar from "../components/Navbar";
-import Nav from "../components/Nav";
+import Navbar from "../../components/Navbar";
 
 function Copyright(props) {
   return (
@@ -82,9 +80,64 @@ export default function SignIn() {
             alignItems: "center",
           }}
         >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
           <Typography component="h1" variant="h5">
-            Home Page
+            Sign in
           </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            {/* <h1>height: {height} width: {width}</h1> */}
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
