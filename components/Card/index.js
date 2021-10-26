0e27e459b948ebfb49009bf04ea8202771ed25d8
Nirel Ibarra <62606372+knee-rel@users.noreply.github.com>
2021-10-26 12:34:28 +0800

    rating component

--- components/Card/index.js
+++ components/Card/index.js
@@ -11,6 +11,10 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import RatingCmp from "../RatingCmp";
+import { ProductionQuantityLimits } from "@mui/icons-material";
+
+import StarIcon from "@mui/icons-material/Star";
 
 const CardComponent = ({ service }) => {
   return (
@@ -21,7 +25,13 @@ const CardComponent = ({ service }) => {
           <Typography gutterBottom variant="h5" component="div">
             {service.serviceType}
           </Typography>
-          <Typography>{service.rating}</Typography>
+          <Typography>
+            <RatingCmp
+              value={service.rating}
+              text={`${service.numReviews} reviews`}
+            />
+          </Typography>
+          {/* <Typography>{service.rating}</Typography> */}
           <Typography variant="body2" color="text.secondary">
             {service.description}
           </Typography>
@@ -30,6 +40,7 @@ const CardComponent = ({ service }) => {
           <Button size="small">Share</Button>
           <Button size="small">Learn More</Button>
         </CardActions>
+        {/* <RatingCmp /> */}
       </Card>
     </Link>
   );
